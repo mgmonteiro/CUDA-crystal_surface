24e1b861b70fa503cb5ab86dd39367c2668b3945
mgmonteiro <maxweljr@gmail.com>
2018-01-29 18:54:16 -0200

    Update CUDA_Surface_vs2.0.cu

--- CUDA_Surface_vs2.0.cu
+++ CUDA_Surface_vs2.0.cu
@@ -19,6 +19,19 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 }
 
 /*
+   Copyright 2018 Maxwel Gama Monteiro Junior
+   
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+limitations under the License.
 **************************************************************************************************
 Author: Maxwel Gama Monteiro Junior
 Contact: maxweljr@gmail.com
